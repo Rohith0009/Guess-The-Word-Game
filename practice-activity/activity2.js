42df9accab5c248b2ce8578cab24417238fb79da
Rohith M <rohithmerala@gmail.com>
2022-01-24 17:02:04 +0530

    Delete practice-activity directory

--- practice-activity/activity2.js
+++
@@ -1,8 +0,0 @@
-function back(){
-    window.location.replace("activity_1.html");
-}
-
-function getscore(){
-    score = localStorage.getItem("Score");
-    document.getElementById("update").innerHTML = "Score: "+score
-}
